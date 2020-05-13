9ccae1bb0969d30d8d581eddc04ceb496a24b08b
unknown <shihan1@staff.sina.com.cn>
2020-05-13 16:47:44 +0800

    修复配置文件问题

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php if(!defined('ROOT')) die('Access denied.');
+
+$servername  = '1';
+$dbname      = '1';
+$dbusername  = '1';
+$dbpassword  = '2';
+$dbmysql  = 'mysqli';
+
+define('TABLE_PREFIX', 'hong_');
+define('COOKIE_KEY', 'F0N3suuvTyXg');
+define('WEBSITE_KEY', '0vT46695MQ8v');
+define('SYSDIR', '/');
+
+?>
